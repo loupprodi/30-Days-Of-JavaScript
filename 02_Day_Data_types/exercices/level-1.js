0b6f786ef2d4429eacd3cf794748eb277a7752fc
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2021-06-22 16:21:35 -0300

    add ex level 1 day 2

--- 02_Day_Data_types/exercices/level-1.js
+++ 02_Day_Data_types/exercices/level-1.js
@@ -1,16 +1,34 @@
-//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+//1- Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+const challenge = '30 days of challenge';
 
-const challenge= "30 days of challenge";
-
-//Print the string on the browser console using console.log()
+//2- Print the string on the browser console using console.log()
 console.log(challenge);
 
-//Print the length of the string on the browser console using console.log()
+//3- Print the length of the string on the browser console using console.log()
 console.log(challenge.length);
 
-//Change all the string characters to capital letters using toUpperCase() method
-console.log("challenger".toUpperCase()); // CHALLENGER
+//4- Change all the string characters to capital letters using toUpperCase() method
+console.log('challenger'.toUpperCase()); // CHALLENGER
 
-//Change all the string characters to lowercase letters using toLowerCase() method
-console.log("CHALLEGENGER".toLowerCase()); // challenger
+//5- Change all the string characters to lowercase letters using toLowerCase() method
+console.log('CHALLEGENGER'.toLowerCase()); // challenger
 
+//6- Cut (slice) out the first word of the string using substr() or   method
+console.log('challenger'.substring(2, 7));
+
+//7- Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log('30 Days Of JavaScript'.substring(3, 6));
+
+//8- Check if the string contains a word Script using includes() method
+console.log(challenge.includes('30 days of challenge'));
+
+//9- Split the string into an array using split() method
+const divisao = challenge.split();
+console.log(divisao);
+
+//10- 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+const companias = ['Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'];
+console.log(companias);
+
+//11-Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+let mudar = challenge.replace(/JavaScript/g, 'Python');
