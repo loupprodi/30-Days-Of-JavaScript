54d85deaea27add9831f566f358b89d4a423fe9f
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2021-06-22 13:47:48 -0300

    add ex level 1 day 2

---
+++ 02_Day_Data_types/exercices/level-1.js
@@ -0,0 +1,16 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+const challenge= "30 days of challenge";
+
+//Print the string on the browser console using console.log()
+console.log(challenge);
+
+//Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+
+//Change all the string characters to capital letters using toUpperCase() method
+console.log("challenger".toUpperCase()); // CHALLENGER
+
+//Change all the string characters to lowercase letters using toLowerCase() method
+console.log("CHALLEGENGER".toLowerCase()); // challenger
+
