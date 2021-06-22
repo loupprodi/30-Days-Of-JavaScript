04bb041bd28bbd5218704c32283700a2620b5034
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2021-06-22 16:24:48 -0300

    add ex level 1 day 2

--- 02_Day_Data_types/exercices/level-1.js
+++ 02_Day_Data_types/exercices/level-1.js
@@ -32,3 +32,12 @@ console.log(companias);
 
 //11-Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
 let mudar = challenge.replace(/JavaScript/g, 'Python');
+
+//13 - What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+console.log(challenge.charAt(15));
+
+// 14 - What is the character code of J in '30 Days Of JavaScript' string using charCodeAt().
+console.log(challenge.charCodeAt('J'));
+
+// 15 - Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript.
+console.log(challenge.indexOf('a'));
