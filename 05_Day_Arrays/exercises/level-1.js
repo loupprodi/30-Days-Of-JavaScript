0fa8b908f6d0f7ac7fc0c13a0d24b32dcfa6fb42
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2021-06-29 16:35:27 -0300

    add ex level 5 day 1

---
+++ 05_Day_Arrays/exercises/level-1.js
@@ -0,0 +1,58 @@
+// exercises level-1
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+];
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
+//1- Declare an empty array;
+const arr = Array(); // criando uma array vazia e pode ser com []
+console.log(arr);
+
+//2- Declare an array with more than 5 number of elements
+const arrNumber = Array(1, 2, 3, 4, 5);
+console.log(arrNumber);
+
+//3- Find the length of your array
+console.log(arrNumber.length);
+
+//4- Get the first item, the middle item and the last item of the array
+console.log(arrNumber[0]);
+
+//5- Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+//6- Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+//7- Print the array using console.log()
+//8- Print the number of companies in the array
+//9- Print the first company, middle and last company
+//10- Print out each company
+//11- Change each company name to uppercase one by one and print them out
+//12- Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+//13- Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+//14- Filter out companies which have more than one 'o' without the filter method
+//15- Sort the array using sort() method
+//16- Reverse the array using reverse() method
+//17- Slice out the first 3 companies from the array
+//18- Slice out the last 3 companies from the array
+//19- Slice out the middle IT company or companies from the array
+//20- Remove the first IT company from the array
+//21- Remove the middle IT company or companies from the array
+//22- Remove the last IT company from the array
+//23- Remove all IT companies
